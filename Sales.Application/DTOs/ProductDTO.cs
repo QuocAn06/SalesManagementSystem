54820116e3e78a3f61c1a6311e1de9328aa1b86e
Le Nguyen Quoc An <lenguyenquocan116@gmail.com>
2025-07-17 23:02:22 +0700

    Create ProductService in Sales.Infrastructure/Services/

---
+++ Sales.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.Application.DTOs
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal CurrentPrice { get; set; }
+        public int StockQuantity { get; set; }
+    }
+}
