1e17320cd06c56f58c562a1a79730180f9f9ac66
Le Nguyen Quoc An <lenguyenquocan116@gmail.com>
2025-07-25 20:19:51 +0700

    update the OrderDetail.cs

--- Sales.Domain/Entities/OrderDetail.cs
+++ Sales.Domain/Entities/OrderDetail.cs
@@ -11,9 +11,10 @@ namespace Sales.Domain.Entities
     {
         public int OrderId { get; set; }
         public int ProductId { get; set; }
+
         public int Quantity { get; set; }
         public decimal UnitPrice { get; set; }
-        public decimal ToalPrice { get; set; }
+        public decimal TotalPrice { get; set; }
 
         public Order Order { get; set; }
         public Product Product { get; set; }
